faefd9296703d732e1588f59fed476ef7bfd83ae
Mustafa Khan <mustafakhanneo@gmail.com>
2024-05-21 15:53:12 +0500

    eas update fix bugs

--- app/(tabs)/about.jsx
+++ app/(tabs)/about.jsx
@@ -21,6 +21,8 @@ const about = () => {
       if (update.isAvailable) {
         await Updates.fetchUpdateAsync();
         await Updates.reloadAsync();
+      } else {
+        alert("You are Running TVM Finance Updated version")
       }
     } catch (error) {
       // You can also add an alert() to see the error message in case of an error when fetching updates.

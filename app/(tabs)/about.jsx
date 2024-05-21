3b58ade8e9f92e01d37761b09a028571748b9a63
Mustafa Khan <mustafakhanneo@gmail.com>
2024-05-21 18:20:44 +0500

    add modal fix bugs

--- app/(tabs)/about.jsx
+++ app/(tabs)/about.jsx
@@ -4,15 +4,45 @@ import {
   TouchableOpacity,
   ScrollView,
   Linking,
-  Image
+  Image,
 } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
-import React from 'react';
+import React, { useState } from 'react';
 import { icons } from "../../constants";
 import { StatusBar } from 'expo-status-bar';
 import * as Updates from 'expo-updates';
+import { Modal, Portal, PaperProvider } from 'react-native-paper';
 
 const about = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [isupdate, setIsupdate] = useState(true);
+  const [closeText, setCloseText] = useState("")
+  const [updateText, setUpdateText] = useState("");
+
+  const closeModal = () => {
+    setModalVisible(false)
+  }
+
+  async function checkUpdates() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        setUpdateText("Update is Available for version 1.0.0");
+        setCloseText("Later")
+        setIsupdate(true);
+        setModalVisible(true);
+      } else {
+        setUpdateText("You are using TVM Finance Updated version 1.0.0");
+        setCloseText("Close")
+        setIsupdate(false);
+        setModalVisible(true);
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      alert(`Check your internet connection: ${error}`);
+    }
+  }
 
   async function onFetchUpdateAsync() {
     try {
@@ -21,24 +51,54 @@ const about = () => {
       if (update.isAvailable) {
         await Updates.fetchUpdateAsync();
         await Updates.reloadAsync();
+        setModalVisible(false);
       } else {
         alert("You are Running TVM Finance Updated version")
       }
     } catch (error) {
       // You can also add an alert() to see the error message in case of an error when fetching updates.
-      alert(`Error fetching latest Expo update: ${error}`);
+      alert(`Check your internet connection: ${error}`);
     }
   }
-
 
   return (
     <SafeAreaView className="bg-[#1D232A] h-full">
       <ScrollView>
+        
       <View className="flex flex-col p-2 mt-4 space-y-4">
+      <PaperProvider>
       <View className="flex flex-col justify-start space-y-2">
+      
+      <Portal>
+        <Modal visible={modalVisible} onDismiss={() => {setModalVisible(false)} } className="">
+          <View
+        className="flex flex-col bg-[#191E24] justify-center border border-[#383F47] items-center rounded-lg h-auto px-2 pt-2 pb-4 space-y-4">
+          <Text className="text-[#A6ADBB] text-md font-medium" >{updateText}</Text>
+          <View className="flex flex-row space-x-4 justify-stretch">
+          <TouchableOpacity
+          className={`flex justify-center rounded-lg items-center ${isupdate ? "w-auto"  : "w-full"} border border-[#383F47] px-4 pt-2 pb-2`}
+          onPress={closeModal}
+        >
+          <Text className=" text-[#A6ADBB]">{closeText}</Text>
+        </TouchableOpacity>
+        {isupdate && ( 
+
+        <TouchableOpacity
+          className="flex justify-center rounded-lg items-center w-auto border border-[#383F47] px-4 pt-2 pb-2"
+          onPress={onFetchUpdateAsync}
+        >
+          <Text className=" text-[#A6ADBB] font-medium">Update Now</Text>
+        </TouchableOpacity>
+        )}
+          </View>
+          
+      </View>
+          </Modal>
+          </Portal>
+          
       <TouchableOpacity
           className="flex flex-row justify-center rounded items-center w-auto border border-[#383F47] pt-2 pb-2"
-          onPress={onFetchUpdateAsync}
+          onPress={checkUpdates}
         >
           
           <Text className=" text-[#A6ADBB]">Check for updates</Text>
@@ -62,6 +122,7 @@ const about = () => {
           goals with confidence.
         </Text>
       </View>
+      </PaperProvider>
       <View className="grid sm:grid-cols-6 grid-cols-1 gap-2">
         <TouchableOpacity
           className="flex flex-row justify-center rounded items-center w-auto border border-[#383F47] pt-2 pb-2"
@@ -119,6 +180,7 @@ const about = () => {
           <Text className=" text-[#A6ADBB]">Mustafa Khan</Text>
         </TouchableOpacity>
       </View>
+      
       <View className="flex flex-col justify-start space-y-2 pt-4">
         <Text className="font-medium text-[#A6ADBB] font-tanker text-2xl mb-2">
           Website Limitations
@@ -143,7 +205,7 @@ const about = () => {
         </View>
       </View>
     </View>
-
+    
       </ScrollView>
     <StatusBar backgroundColor="#1D232A" style='light'/>
     </SafeAreaView>
